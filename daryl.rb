70d23f24d5d3a80adcf4110b93449d24ec9fab34
Gilles Crofils <gilles@secondbureau.com>
2010-12-29 17:34:42 +0100

    work in progress

--- daryl.rb
+++ daryl.rb
@@ -1,5 +1,48 @@
 require 'rubygems'
 require 'sinatra'
+require 'data_mapper'
+require 'syntaxi'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
+
+### MODELS
+
+class Bot < DataMapper::base
+  property :name, :string
+  property :signature, :string
+  property :created_at, :datetime
+  property :updated_at, :datetime
+end
+
+class Os < DataMapper::base
+  property :name, :string
+  property :signature, :string
+  property :created_at, :datetime
+  property :updated_at, :datetime
+end
+
+class Account < DataMapper::base
+  property :domain, :string
+  property :gacode, :string
+  property :utma  , :string
+  property :created_at, :datetime
+  property :updated_at, :datetime
+end
+
+class Page < DataMapper::base
+  property :domain, :string  
+  property :agent, :string
+  property :uri, :text
+  property :sent_at :datetime
+  property :return_code, :string
+  property :created_at, :datetime
+  property :updated_at, :datetime
+end
+
+database.table_exists?(Bot) or database.save(Bot)
+
+
+### CONTROLLER ACTIONS
 
 get '/' do 
   "Hello from sinatra on heroku!"
